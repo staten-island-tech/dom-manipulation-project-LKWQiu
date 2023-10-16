af87174005f23daccc6c766f236e57df953dab5c
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-10-16 09:24:57 -0400

    sdvz

---
+++ app.js
@@ -0,0 +1,11 @@
+const DOMselectors= {
+    form: document.querySelector("#form"),
+    firstName: document.querySelector(".firstName"),
+    h2: document.querySelectorAll("h2")
+};
+DOMselectors.#form.addEventListener("submit", function(event){
+    event.preventDefault();
+    console.log(DOMselectors.firstName.value);
+    DOMselectors.h2.forEach(
+        (e1) => e1.textContent = DOMselectors.firstName.value);
+});
