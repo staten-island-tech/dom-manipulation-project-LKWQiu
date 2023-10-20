1e4ffec82ecaa1d310203d50c714fa8380bcbc88
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-10-20 08:50:25 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const DOMselectors= {
     firstName: document.querySelector(".firstName"),
     h2: document.querySelectorAll("h2")
 };
-DOMselectors.#form.addEventListener("submit", function(event){
+DOMselectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMselectors.firstName.value);
     DOMselectors.h2.forEach(
-        (e1) => e1.textContent = DOMselectors.firstName.value);
+        (e1) => {e1.textContent = DOMselectors.firstName.value});
 });
