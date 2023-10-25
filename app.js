c0ea08b74d657d232608ec9ee39b9d89b4da7c35
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-10-25 10:17:12 -0400

    cof

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-const DOMselectors= {
-    form: document.querySelector("#form"),
-    firstName: document.querySelector(".firstName"),
-    h2: document.querySelectorAll("h2"),
-};
-DOMselectors.form.addEventListener("submit", function(event){
-    event.preventDefault();
-    console.log(DOMselectors.firstName.value);
-    DOMselectors.h2.forEach(
-<<<<<<< HEAD
-        (e1) => {e1.textContent = DOMselectors.firstName.value});
-=======
-        (h2) => (h2.textContent = DOMselectors.firstName.value)
-    );
->>>>>>> 4790d45bcb67961e7b4a8f416bd5be913c2b165b
+document.addEventListener("DOMContentLoaded", function() {
+    function DOMselectors(){
+        var form1 = document.getElementById("form1");
+        var firstName = form1.elements["firstName"].value;
+        var Date = form1.elements["Date"].value;
+        var Class = form1.elements["Class"].value;
+        return {
+            firstName: firstName,
+            Date: Date,
+            Class: Class
+        };
+    };
+    
+
+    document.getElementById("submitButton").addEventListener("click", function(event) {
+        event.preventDefault();
+        
+        var formValues = DOMselectors();
+
+        document.getElementById("in1").innerHTML = formValues.firstName;
+        document.getElementById("in2").innerHTML = formValues.Date;
+        document.getElementById("in3").innerHTML = formValues.Class;
+    });
 });
