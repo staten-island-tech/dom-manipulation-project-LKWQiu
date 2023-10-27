7feccd5300dcda1bdf42e1be1ae3d60088dd0322
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-10-27 09:25:01 -0400

    a

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
 
 const DOMselectors = {
     form1 : document.getElementById("form1"),
-    fistName : document.getElementById("firstName"),
+    Name : document.getElementById("Name"),
     Date : document.getElementById("Date"),
     Class : document.getElementById("Class"),
-    Image: document.getElementById("Image"),
+    Image: document.getElementById(".Image"),
+    card: document.querySelector(".card"),
 };
 function callvalue(){
-    const callfirstName = DOMselectors.fistName.value;
+    const callName = DOMselectors.Name.value;
     const callDate = DOMselectors.Date.value;
     const callClass = DOMselectors.Class.value;
     const callImage = DOMselectors.Image.value;
 
     return{
-        firstName: callfirstName,
+        Name: callName,
         Date: callDate,
         Class: callClass,
         Image: callImage,
 
     };
 }
+function copypaste(x){
+    const outBox = `<div class="card" id="card">
+    <img src="${x.Image}" alt="Error">
+    <p>Name:${x.Name}</p>
+    <p>Class:${x.Class}</p>
+    <p>Date:${x.Date}</p>
+    </div>`;
+    DOMselectors.card.insertAdjacentHTML("beforeend", outBox);
+};
+
+DOMselectors.form1.addEventListener("submit", function(event){
+    event.preventDefault();
+    const newcard = callvalue();
+    copypaste(newcard);
+});
